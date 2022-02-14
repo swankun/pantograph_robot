8726da93f06dff53f1a137294a02d1aed188cef4
Wankun Sirichotiyakul <28937533+swankun@users.noreply.github.com>
2022-02-14 13:48:09 -0700

    Update bringup.sh

--- pantograph_hardware/script/bringup.sh
+++ pantograph_hardware/script/bringup.sh
@@ -4,5 +4,4 @@ echo "Restarting EtherCAT master services."
 sudo /etc/init.d/ethercat stop
 sudo /etc/init.d/ethercat start
 
-source "/home/rcluser/Projects/ros/ethercat_ws/devel/setup.bash"
 roslaunch pantograph_hardware bringup.launch
